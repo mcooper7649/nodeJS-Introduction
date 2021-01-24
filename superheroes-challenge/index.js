6b8cc4ae52766d1a0a261438469280974d76153e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-24 12:46:58 -0500

    completed supervillain challenge

--- superheroes-challenge/index.js
+++ superheroes-challenge/index.js
@@ -1,5 +1,9 @@
 const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+const suppervillains = require('supervillains');
 
 var mySuperHeroName = superheroes.random();  //superheroes calls the module while random is a default method available
+var mySuperVillainName = supervillains.random();
 
-console.log(mySuperHeroName); // log our superHeroName
+
+console.log(mySuperHeroName + " will fight " + mySuperVillainName); // log our superHeroName
