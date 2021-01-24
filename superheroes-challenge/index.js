f7e4628191d205b0d0102d8389053ac369009147
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-24 12:37:43 -0500

    Completed NPM Challenge Superheroes

---
+++ superheroes-challenge/index.js
@@ -0,0 +1,5 @@
+const superheroes = require('superheroes');
+
+var mySuperHeroName = superheroes.random();  //superheroes calls the module while random is a default method available
+
+console.log(mySuperHeroName); // log our superHeroName
